f4c00a9d4f6c14b028a9e40ee1969c43b501e168
Matt Pitt <matt.ianpitt@gmail.com>
2018-11-16 16:34:48 +0100

    style dropdown menu and add new links

--- app/javascript/components/navbar_dropdown.js
+++ app/javascript/components/navbar_dropdown.js
@@ -1,14 +1,13 @@
 document.querySelector(".application-nav-mobile-icon").addEventListener("click", (event) => {
   event.preventDefault();
-  console.log("1 works")
   let dropdownList = document.querySelector(".application-nav-dropdown");
   if (dropdownList.style.display === "none") {
-    console.log("display block now")
     dropdownList.style.display = "block";
   } else {
     dropdownList.style.display = "none";
   }
+
 });
 
-console.log("hi");
+
 
